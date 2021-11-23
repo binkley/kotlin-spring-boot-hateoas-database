af9fb7f46bbddbb3695e27bd666dd9bf4606e951
Brian Oxley <boxley@thoughtworks.com>
2021-11-24 07:15:57 +0800

    Actual integration tests

--- src/test/kotlin/hm/binkley/labs/hateoas/HateoasApplicationIT.kt
+++ src/test/kotlin/hm/binkley/labs/hateoas/HateoasApplicationIT.kt
@@ -2,10 +2,35 @@ package hm.binkley.labs.hateoas
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import org.springframework.boot.web.server.LocalServerPort
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse.BodyHandlers.ofString
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class HateoasApplicationIT {
+    @LocalServerPort
+    var port: Int = 0
+
+    val client = HttpClient.newHttpClient()
+
     @Test
-    fun contextLoads() {
+    fun `should have a thingy`() {
+        get("/data/thingies/1").body()
     }
+
+    @Test
+    fun `should have info endpoint`() {
+        get("/admin/info").body()
+    }
+
+    private fun get(path: String) = client.send(
+        HttpRequest.newBuilder()
+            .GET()
+            .uri(URI.create("http://localhost:$port$path"))
+            .build(),
+        ofString()
+    )
 }
