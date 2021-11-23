56c60091c3377bfdb29a8776dda1a956987e31b5
Brian Oxley <boxley@thoughtworks.com>
2021-11-23 13:12:05 +0800

    Init from Spring Initializr

---
+++ src/main/kotlin/hm/binkley/labs/hateoas/HateoasApplication.kt
@@ -0,0 +1,11 @@
+package hm.binkley.labs.hateoas
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HateoasApplication
+
+fun main(args: Array<String>) {
+	runApplication<HateoasApplication>(*args)
+}
---
+++ src/test/kotlin/hm/binkley/labs/hateoas/HateoasApplicationTests.kt
@@ -0,0 +1,13 @@
+package hm.binkley.labs.hateoas
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HateoasApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
