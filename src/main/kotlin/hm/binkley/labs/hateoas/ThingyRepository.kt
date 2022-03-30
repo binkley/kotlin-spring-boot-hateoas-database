8c8f0337ef167bd2d74bc803bdf53657f726fd4d
Brian Oxley <boxley@thoughtworks.com>
2022-03-30 11:23:18 -0500

    Make IntelliJ happier

--- src/main/kotlin/hm/binkley/labs/hateoas/ThingyRepository.kt
+++ src/main/kotlin/hm/binkley/labs/hateoas/ThingyRepository.kt
@@ -2,4 +2,5 @@ package hm.binkley.labs.hateoas
 
 import org.springframework.data.repository.PagingAndSortingRepository
 
+@Suppress("unused")
 interface ThingyRepository : PagingAndSortingRepository<Thingy, String>
