ea77c5cd44954c3c11fef28c2aa3e4a1b117da62
Brian Oxley <boxley@thoughtworks.com>
2022-08-04 14:08:22 -0400

    Hygiene

--- src/main/kotlin/hm/binkley/labs/AuthorRepository.kt
+++ src/main/kotlin/hm/binkley/labs/AuthorRepository.kt
@@ -2,5 +2,4 @@ package hm.binkley.labs
 
 import org.springframework.data.repository.PagingAndSortingRepository
 
-@Suppress("unused")
 interface AuthorRepository : PagingAndSortingRepository<Author, String>
--- src/main/kotlin/hm/binkley/labs/BookRepository.kt
+++ src/main/kotlin/hm/binkley/labs/BookRepository.kt
@@ -2,5 +2,4 @@ package hm.binkley.labs
 
 import org.springframework.data.repository.PagingAndSortingRepository
 
-@Suppress("unused")
 interface BookRepository : PagingAndSortingRepository<Book, String>
