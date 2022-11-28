48a1cea3e2dd8978aaa097e113c10385d1dc4789
Brian Oxley <boxley@thoughtworks.com>
2022-11-28 15:31:27 -0600

    Bump Spring Boot to 3

--- src/main/kotlin/hm/binkley/labs/AuthorRepository.kt
+++ src/main/kotlin/hm/binkley/labs/AuthorRepository.kt
@@ -1,5 +1,7 @@
 package hm.binkley.labs
 
+import org.springframework.data.repository.CrudRepository
 import org.springframework.data.repository.PagingAndSortingRepository
 
-interface AuthorRepository : PagingAndSortingRepository<Author, String>
+interface AuthorRepository :
+    CrudRepository<Author, String>, PagingAndSortingRepository<Author, String>
--- src/main/kotlin/hm/binkley/labs/BookRepository.kt
+++ src/main/kotlin/hm/binkley/labs/BookRepository.kt
@@ -1,5 +1,7 @@
 package hm.binkley.labs
 
+import org.springframework.data.repository.CrudRepository
 import org.springframework.data.repository.PagingAndSortingRepository
 
-interface BookRepository : PagingAndSortingRepository<Book, String>
+interface BookRepository :
+    CrudRepository<Book, String>, PagingAndSortingRepository<Book, String>
